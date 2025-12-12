36684abaf15cea794fc6ef9d404daaafecb79fa5
Cyril HILLION <chillion@f2r2s12.clusters.42paris.fr>
2025-12-12 12:42:12 +0100

    (30) fix ex00 code error

--- 30/ex00/watchtower.ml
+++ 30/ex00/watchtower.ml
@@ -10,6 +10,6 @@ module Watchtower =
   struct
     type hour = int
     let zero = 12
-    let add h1 h2 = let result = ((h1 mod zero) + (h2 mod zero) + zero + zero) mod zero in if result == 0 then zero else result
-    let sub h1 h2 = let result = ((h1 mod zero) - (h2 mod zero) + zero + zero) mod zero in if result == 0 then zero else result
+    let add h1 h2 = let result = ((h1 mod zero) + (h2 mod zero) + zero + zero) mod zero in if result = 0 then zero else result
+    let sub h1 h2 = let result = ((h1 mod zero) - (h2 mod zero) + zero + zero) mod zero in if result = 0 then zero else result
   end
